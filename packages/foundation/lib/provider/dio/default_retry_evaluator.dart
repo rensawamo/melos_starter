9f535877730e4ebeaf7463100e1f70dbd45db944
rensawamo <ren0112ngzk@gmail.com>
2024-11-17 15:27:35 +0900

    fix: gorouterが動かなくなった問題

---
+++ packages/foundation/lib/provider/dio/default_retry_evaluator.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+
+typedef RetryEvaluator = FutureOr<bool> Function(
+  DioException error,
+  int attempt,
+);
+
+class DefaultRetryEvaluator {
+  DefaultRetryEvaluator(this._retryStatuses);
+
+  final Set<int> _retryStatuses;
+  int currentAttempt = 0;
+
+  FutureOr<bool> evaluate(DioException error, int attempt) {
+    bool shouldRetry;
+    if (error.type == DioExceptionType.badResponse) {
+      final statusCode = error.response?.statusCode;
+      if (statusCode != null) {
+        shouldRetry = isRetryable(statusCode);
+      } else {
+        shouldRetry = true;
+      }
+    } else {
+      shouldRetry = error.type != DioExceptionType.cancel &&
+          error.error is! FormatException;
+    }
+    currentAttempt = attempt;
+    return shouldRetry;
+  }
+
+  bool isRetryable(int statusCode) => _retryStatuses.contains(statusCode);
+}
---
+++ packages/foundation/lib/provider/dio/retry_status.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+const defaultRetryableStatuses = <int>{
+  HttpStatus.requestTimeout, // 408
+  HttpStatus.tooManyRequests, // 429
+  HttpStatus.internalServerError, // 500
+  HttpStatus.badGateway, // 502
+  HttpStatus.serviceUnavailable, // 503
+  HttpStatus.gatewayTimeout, // 504
+  440, // Login Timeout (IIS-specific)
+  499, // Client Closed Request (Nginx-specific)
+  460, // Client Closed Request (AWS-specific)
+  598, // Network Read Timeout Error (non-standard)
+  599, // Network Connect Timeout Error (non-standard)
+  520, // Web Server Returned Unknown Error (Cloudflare)
+  521, // Web Server Is Down (Cloudflare)
+  522, // Connection Timed Out (Cloudflare)
+  523, // Origin Is Unreachable (Cloudflare)
+  524, // Timeout Occurred (Cloudflare)
+  525, // SSL Handshake Failed (Cloudflare)
+  527, // Railgun Error (Cloudflare)
+};
+
+@Deprecated('Use [defaultRetryableStatuses]')
+const retryableStatuses = defaultRetryableStatuses;
+
+bool isRetryable(int statusCode) =>
+    defaultRetryableStatuses.contains(statusCode);
